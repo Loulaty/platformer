7dd48e3ebcf315612c5eb38f2001a5b3631f5f89
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-02-09 16:56:05 +0100

    Update monstre3.js

--- platformer-main/src/objects/monstre3.js
+++ platformer-main/src/objects/monstre3.js
@@ -9,11 +9,6 @@ class monstre3 extends ObjetEnnemi{
         super(scene, x, y, "monstre3");
         this.body.allowGravity=true;
 
-        this.setBodySize(this.body.width-400,this.body.height-400);
-        this.setOffset(150, 250);
-
-        this.setOrigin(10,0);
-
         this.setDisplaySize(64,64);
         this.setVelocityX(200);
         this.setCollideWorldBounds(true);
