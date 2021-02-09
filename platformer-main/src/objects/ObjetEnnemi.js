b597317ccbb53a0b4406e90cf7f772b7b2504587
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-02-09 18:36:26 +0100

    commit

--- platformer-main/src/objects/ObjetEnnemi.js
+++ platformer-main/src/objects/ObjetEnnemi.js
@@ -11,7 +11,7 @@ class ObjetEnnemi extends ObjetPhysique{
         scene.physics.add.overlap(
             scene.player,
             this,
-            scene.hitSpike,
+            scene.hitMonster,
             null,
             scene
         );
--- platformer-main/src/scenes/structure/Tableau.js
+++ platformer-main/src/scenes/structure/Tableau.js
@@ -16,6 +16,7 @@ class Tableau extends Phaser.Scene{
      */
     preload(){
         this.load.image('sky', 'assets/sky.png');
+        this.load.image('blood', 'assets/blood.png');
         this.load.image('spike', 'assets/spike.png');
         this.load.spritesheet('player',
             'assets/player.png',
@@ -39,10 +40,44 @@ class Tableau extends Phaser.Scene{
          */
         this.player=new Player(this,0,0);
 
+        this.blood=this.add.sprite(this.sys.canvas.width/2,this.sys.canvas.height/2,"blood")
+        this.blood.displayWidth=64;
+        this.blood.displayHeight=64;
+        this.blood.visible=false;
+
     }
     update(){
         super.update();
         this.player.move();
+    }
+
+    /**
+     *
+     * @param {Sprite} object Objet qui saigne
+     * @param {function} onComplete Fonction à appeler quand l'anim est finie
+     */
+    saigne(object,onComplete){
+        let me=this;
+        me.blood.visible=true;
+        me.blood.rotation = Phaser.Math.Between(0,6);
+        me.blood.x=object.x;
+        me.blood.y=object.y;
+        me.tweens.add({
+            targets:me.blood,
+            duration:200,
+            displayHeight:{
+                from:40,
+                to:70,
+            },
+            displayWidth:{
+                from:40,
+                to:70,
+            },
+            onComplete: function () {
+                me.blood.visible=false;
+                onComplete();
+            }
+        })
     }
 
     ramasserEtoile (player, star)
@@ -62,13 +97,6 @@ class Tableau extends Phaser.Scene{
         if(totalActive===0){
             this.win();
         }
-        /*
-        // this.stars est un groupe (plus tard)
-        if (this.stars.countActive(true) === 0)
-        {
-           this.win();
-        }
-         */
     }
 
     /**
@@ -82,6 +110,52 @@ class Tableau extends Phaser.Scene{
         player.setTint(0xff0000);
         player.anims.play('turn');
         this.scene.restart();
+
+    }
+
+    /**
+     * Quand on touche un monstre
+     * si on le touche par en haut on le tue, sinon c'est lui qui nous tue
+     * @param {Player} player
+     * @param {Phaser.Physics.Arcade.Sprite} monster
+     */
+    hitMonster(player, monster){
+        let me=this;
+        if(monster.isDead !== true){ //si notre monstre n'est pas déjà mort
+            if(
+                // si le player descend
+                player.body.velocity.y > 0
+                // et si le bas du player est plus haut que le monstre
+                && player.getBounds().bottom < monster.getBounds().top+30
+
+            ){
+                ui.gagne();
+                monster.isDead=true; //ok le monstre est mort
+                monster.disableBody(true,true);//plus de collisions
+                this.saigne(monster,function(){
+                    //à la fin de la petite anim...ben il se passe rien :)
+                })
+                //notre joueur rebondit sur le monstre
+                player.directionY=500;
+            }else{
+                //le joueur est mort
+                if(!me.player.isDead){
+                    me.player.isDead=true;
+                    me.player.visible=false;
+                    //ça saigne...
+                    me.saigne(me.player,function(){
+                        //à la fin de la petite anim, on relance le jeu
+                        me.blood.visible=false;
+                        me.player.anims.play('turn');
+                        me.player.isDead=false;
+                        me.scene.restart();
+                    })
+
+                }
+
+
+            }
+        }
 
     }
 
