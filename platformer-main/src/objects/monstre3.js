ab1d5781b0d8bc1f493410976cbe1353c0ff937a
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-02-11 12:53:37 +0100

    bouce

--- platformer-main/src/objects/monstre3.js
+++ platformer-main/src/objects/monstre3.js
@@ -12,7 +12,7 @@ class monstre3 extends ObjetEnnemi{
         this.setDisplaySize(64,64);
         this.setVelocityX(200);
         this.setCollideWorldBounds(true);
-        this.setBounce(1.1);
+        this.setBounce(1);
     }
 
 }
