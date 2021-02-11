1bd7cd9d5e00f856991caf7f6f2beababe07bd5b
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-02-11 12:51:00 +0100

    bouce

--- platformer-main/src/objects/monstreviolet.js
+++ platformer-main/src/objects/monstreviolet.js
@@ -12,7 +12,7 @@ class monstreviolet extends ObjetEnnemi{
         this.setDisplaySize(64,64);
         this.setVelocityX(50);
         this.setCollideWorldBounds(true);
-        this.setBounce(1);
+        this.setBounce(3);
     }
 
 }
