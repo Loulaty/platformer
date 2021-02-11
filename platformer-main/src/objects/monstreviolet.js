a00a7c748e0dafc4ae503a0977764ad6fbfbb577
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2021-02-11 13:09:01 +0100

    Update monstreviolet.js

--- platformer-main/src/objects/monstreviolet.js
+++ platformer-main/src/objects/monstreviolet.js
@@ -12,7 +12,7 @@ class monstreviolet extends ObjetEnnemi{
         this.setDisplaySize(64,64);
         this.setVelocityX(50);
         this.setCollideWorldBounds(true);
-        this.setBounce(1.0005);
+        this.setBounce(1);
     }
 
 }
